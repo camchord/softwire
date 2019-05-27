c3f55909283ac505c07948b313fd0dac20bd6ec1
Cameron Chisholm <cam.j.chisholm@gmail.com>
2019-05-28 00:05:32 +0100

    Created multiply function

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,14 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
+
+function multiply() {
+    console.log('Enter your first number to multiply');
+    const x = readline.prompt();
+    console.log('Enter your second number to multiply');
+    const y = readline.prompt();
+    const result = +x * +y
+    console.log(x, ' x ', y, ' = ', result);
+}
+
+multiply();
