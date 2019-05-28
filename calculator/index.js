3a8bde865f424a1046d5c52949a5507c149f8958
Cameron Chisholm <cam.j.chisholm@gmail.com>
2019-05-28 23:57:24 +0100

    Updated calculate function to work with multiple numbers, using a single operator

--- calculator/index.js
+++ calculator/index.js
@@ -4,26 +4,32 @@ console.log('Welcome to the calculator!');
 
 function calculate() {
     let operator = readline.question("Please enter the operator: ");
-    let num1 = +readline.question("Please enter the first number: ");
-    let num2 = +readline.question("Please enter the second number: ");
-    let solution;
+    let cb;
     switch (operator) {
         case '+':
-            solution = num1 + num2;
+            cb = (x,y) => x + y;
             break;
         case '-':
-            solution = num1 - num2;
+            cb = (x,y) => x - y;
             break;
         case '*':
-            solution = num1 * num2;
+            cb = (x,y) => x * y;
             break;
         case '/':
-            solution = num1 / num2;
+            cb = (x,y) => x / y;
             break;
         default:
             return console.log('Invalid operator, please try again');
     }
-    console.log(`${num1} ${operator} ${num2} = ${solution}`);
+    let numArr = [];
+    let size = +readline.question(`How many numbers would you like to ${operator}? `);
+    for (let i = 1; i <= size; i++) {
+        let x = +readline.question(`Please enter number ${i}: `);
+        numArr.push(x);
+    }
+    let start = numArr.shift();
+    let solution = numArr.reduce((acc, x) => cb(acc, x), start);
+    console.log(`${start} ${operator}`, numArr.join(` ${operator} `), '=' , solution);
 }
 
 calculate();
