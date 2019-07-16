d6ee11cf63c09b9cb14aefb89a2bf7afc14667fd
Cameron Chisholm <Cameron.Chisholm@softwire.com>
2019-07-16 12:32:38 +0100

    Added functions for listing all accounts, and listing transactions for a single account

--- SupportBank/index.js
+++ SupportBank/index.js
@@ -3,6 +3,8 @@ const moment = require('moment');
 
 let rawData
 
+const accounts = []
+
 class Account {
     constructor(name, transaction) {
         this.name = name;
@@ -14,19 +16,40 @@ class Account {
         this.transactions.push(transaction)
     }
 
-    total() {
+    balance() {
+        this.total = this.transactions.reduce((acc, ts) => {
+            if (ts.From === this.name) {
+                return acc - ts.Amount
+            } else {
+                return acc + ts.Amount
+            }
+        },0).toFixed(2)
+    }
 
+    listAccount() {
+        console.log(`${this.name} - Balance: ${this.total}`);
+        this.transactions.forEach(ts => {
+            console.log(`Date: ${ts.Date.format("DD/MM/YYYY")} - ${ts.From === this.name ? "To" : "From"}: ${ts.From === this.name ? ts.To : ts.From} - Amount: ${ts.From === this.name ? "-" : ""}£${ts.Amount} - Reference: ${ts.Narrative}`)
+        })
+    }
+
+    static listAll() {
+        accounts.forEach(account => console.log(`Name: ${account.name}, Balance: £${account.total}`))
+    }
+
+    static balanceUpdate() {
+        accounts.forEach(account => account.balance())
     }
 }
 
-const accounts = []
+
 
 const createAccounts = (data) => {
     data.forEach((ts) => {
-        let from = ts.From;
-        let to = ts.To;
-        let fromIndex = accounts.findIndex(account => account.name === from);
-        let toIndex = accounts.findIndex(account => account.name === to);
+        const from = ts.From;
+        const to = ts.To;
+        const fromIndex = accounts.findIndex(account => account.name === from);
+        const toIndex = accounts.findIndex(account => account.name === to);
         if (fromIndex === -1) {
             accounts.push(new Account(from, ts))
         } else {
@@ -41,26 +64,25 @@ const createAccounts = (data) => {
 }
 
 const csvParse = (cb) => {
-    console.log(rawData)
-    let data = rawData.split('\n').map((ts) => {
+    const data = rawData.split('\n').map((ts) => {
         entries = ts.split(',');
         return {
             Date: moment(entries[0],'DD/MM/YYYY'),
             From: entries[1],
             To: entries[2],
             Narrative: entries[3],
-            Amount: entries[4]
+            Amount: parseFloat(entries[4])
         }
     });
-    cb(data.slice(1,data.length-1))
-    
+    cb(data.slice(1,data.length-1))   
 }
 
 fs.createReadStream('Transactions2014.csv')
     .on('data', data => rawData = data.toString())
     .on('close', () => csvParse((data) => {
         createAccounts(data);
-        console.log(accounts)
+        Account.balanceUpdate();
+        accounts[3].listAccount();
     }))
 
     
